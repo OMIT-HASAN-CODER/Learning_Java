1cab0098e9143db09c2dbb2da7ed5a1b1a33431e
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-19 23:16:52 +0600

    printing hello java five times.

---
+++ src/main/java/Chapter_01/Print5Times.java
@@ -0,0 +1,11 @@
+package Chapter_01;
+
+public class Print5Times {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        System.out.println("Hello Java!");
+        System.out.println("Hello Java!");
+        System.out.println("Hello Java!");
+        System.out.println("Hello Java!");
+    }
+}
