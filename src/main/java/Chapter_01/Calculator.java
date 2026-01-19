3575bcbfc6a732022c2dad8f6f5f07dbc99e228f
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-20 00:12:01 +0600

    simple calculation.

---
+++ src/main/java/Chapter_01/Calculator.java
@@ -0,0 +1,11 @@
+package Chapter_01;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.print("2 + 3 = ");
+        System.out.println(2 + 3);
+        System.out.println("2 - 3 = " + (2 - 3));
+        System.out.println("2 * 3 = " + (2 * 3));
+        System.out.println("4 / 2 = " + (4 / 2));
+    }
+}
