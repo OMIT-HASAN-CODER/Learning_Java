b880c33b95d1ed82bc97a008b411c594fddac62a
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-21 02:40:16 +0600

    escape sequence demo.

---
+++ src/main/java/Chapter_02/EscapeSequenceDemo.java
@@ -0,0 +1,11 @@
+package Chapter_02;
+
+public class EscapeSequenceDemo {
+    public static void main(String[] args) {
+        String paragraph = "The roaring fans saw \n"
+                + "their beloved \"Tigers\" creat another\n"
+                + "\tbit of history in their favorite \n"
+                + "den last night.";
+        System.out.println(paragraph);
+    }
+}
