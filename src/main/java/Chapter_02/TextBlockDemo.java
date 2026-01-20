874e83efd19d150e025c9ab12434f1518ff66058
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-21 02:44:27 +0600

    text block demo.

---
+++ src/main/java/Chapter_02/TextBlockDemo.java
@@ -0,0 +1,12 @@
+package Chapter_02;
+
+public class TextBlockDemo {
+    public static void main(String[] args) {
+        String paragraph = """
+                The roaring fans saw
+                their beloved "Tigers" create another
+                \tbit of history in their favourite
+                den last night.""";
+        System.out.println(paragraph);
+    }
+}
