c79c911967630f8703ef639aa505dc07c88be566
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-21 02:22:34 +0600

    java to ascii.

---
+++ src/main/java/Chapter_02/JavaToASCII.java
@@ -0,0 +1,9 @@
+package Chapter_02;
+
+public class JavaToASCII {
+    public static void main(String[] args) {
+        char a = 'a';
+        int asciiValue = a;
+        System.out.println(asciiValue);
+    }
+}
