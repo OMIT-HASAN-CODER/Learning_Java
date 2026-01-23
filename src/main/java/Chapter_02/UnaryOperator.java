6861bf1920de50b64eb8999273e92e40316e96ed
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-23 11:50:19 +0600

    unary operator.

---
+++ src/main/java/Chapter_02/UnaryOperator.java
@@ -0,0 +1,17 @@
+package Chapter_02;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int result = +1;
+        System.out.println(result);
+
+        result--;
+        System.out.println(result);
+
+        result++;
+        System.out.println(result);
+
+        result = -result;
+        System.out.println(result);
+    }
+}
