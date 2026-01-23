617034aee62833c1b4c76cd890b178f941ace15a
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-23 12:04:36 +0600

    widening primitive conversion.

---
+++ src/main/java/Chapter_02/WideningPrimitiveConversion.java
@@ -0,0 +1,13 @@
+package Chapter_02;
+
+public class WideningPrimitiveConversion {
+    public static void main(String[] args) {
+        int anInt = 100;
+        long aLong = anInt;
+        float aFloat = aLong;
+
+        System.out.println("Int value " + anInt);
+        System.out.println("Long value " + aLong);
+        System.out.println("Float value " + aFloat);
+    }
+}
