69b33e366fce3788ed8a289bc30803dd207012d9
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-23 11:40:42 +0600

    swap variable.

---
+++ src/main/java/Chapter_02/SwapVariable.java
@@ -0,0 +1,15 @@
+package Chapter_02;
+
+public class SwapVariable {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 22;
+
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
