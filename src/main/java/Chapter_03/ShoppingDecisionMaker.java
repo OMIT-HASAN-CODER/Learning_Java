3befcc87da0d09ae2d2b507c7d3e3619ff73957f
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-24 21:37:23 +0600

    shopping decision maker.

---
+++ src/main/java/Chapter_03/ShoppingDecisionMaker.java
@@ -0,0 +1,58 @@
+package Chapter_03;
+
+import java.util.Scanner;
+
+public class ShoppingDecisionMaker {
+    static final boolean DISCOUNT_AVAILABLE = true;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the price of the product: ");
+        int price = input.nextInt();
+
+        if (isAffordable(price)) {
+            buyProduct();
+        } else if (isLittleExpensive(price)) {
+            if (isDiscountAvailable()) {
+                buyProduct();
+            } else {
+                addThisProductOnWishList();
+            }
+        } else {
+            dontBuyThisProduct();
+        }
+    }
+
+    private static void dontBuyThisProduct() {
+        System.out.println("I will not buy this product.");
+    }
+
+    private  static void addThisProductOnWishList() {
+        System.out.println("I will buy it later.");
+    }
+
+    private static boolean isDiscountAvailable() {
+        return DISCOUNT_AVAILABLE;
+    }
+
+    private static boolean isLittleExpensive(int price) {
+        if (300 < price && price < 1000) {
+            return true;
+        } else {
+            return  false;
+        }
+    }
+
+    private static void buyProduct() {
+        System.out.println("I will buy it.");
+    }
+
+    private static boolean isAffordable(int price) {
+        if (price <= 300) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
