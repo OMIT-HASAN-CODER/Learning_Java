171d29691ec664be0eb859fd0fa1d9076b584ec7
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-26 21:03:14 +0600

    free fall demo.

---
+++ src/main/java/Chapter_03/FreeFall.java
@@ -0,0 +1,25 @@
+package Chapter_03;
+
+import java.util.Scanner;
+
+public class FreeFall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a day between (1 to 7) :");
+        int day = input.nextInt();
+
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekdays");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekends");
+        }
+    }
+}
