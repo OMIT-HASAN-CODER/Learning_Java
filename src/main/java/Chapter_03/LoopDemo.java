a3465cfcdb3190f114206fe03431a86e04218e36
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-26 23:02:14 +0600

    loop demo.

---
+++ src/main/java/Chapter_03/LoopDemo.java
@@ -0,0 +1,11 @@
+package Chapter_03;
+
+public class LoopDemo {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("I love Java Programming.");
+            counter ++;
+        }
+    }
+}
