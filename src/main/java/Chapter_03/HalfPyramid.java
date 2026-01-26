9810b50e576f8d2166a02f90c21f3e59320c5c47
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-27 01:24:51 +0600

    half pyramid.

---
+++ src/main/java/Chapter_03/HalfPyramid.java
@@ -0,0 +1,19 @@
+package Chapter_03;
+
+import java.util.Scanner;
+
+public class HalfPyramid {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
