5f076bc56dee52f16c10a8eb6881d2e24bf6f46e
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-26 22:01:47 +0600

    simple calculator.

---
+++ src/main/java/Chapter_03/Calculator.java
@@ -0,0 +1,29 @@
+package Chapter_03;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Choose an operator: +, -, *, or /: ");
+        char operator = input.next().charAt(0);
+        System.out.println("Enter first number: ");
+        int number1 = input.nextInt();
+        System.out.println("Enter second number: ");
+        int number2 = input.nextInt();
+
+        int result = switch (operator) {
+            case '+' -> number1 + number2;
+            case '-' -> number1 - number2;
+            case '*' -> number1 * number2;
+            case '/' -> number1 / number2;
+            default -> {
+                System.out.println("Invalid operator!");
+                yield 0;
+            }
+        };
+
+        System.out.println("Result: " + result);
+    }
+}
