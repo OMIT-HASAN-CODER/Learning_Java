5bfb14af4fc045a4c74412dbd42199d584f7458f
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-28 01:20:13 +0600

    default value of array.

---
+++ src/main/java/Chapter_04/DefaultValueOfArray.java
@@ -0,0 +1,14 @@
+package Chapter_04;
+
+import java.util.Arrays;
+
+public class DefaultValueOfArray {
+    public static void main(String[] args) {
+        int[] ints = new int[5];
+        System.out.println("ints = " + Arrays.toString(ints));
+        Integer[] integers = new Integer[5];
+        System.out.println("integers = " + Arrays.toString(integers));
+        float[] floats = new float[5];
+        System.out.println("floats = " + Arrays.toString(floats));
+    }
+}
