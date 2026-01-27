655e324cb1afdb47b873b8ae1fe5596dc268234f
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-27 13:01:55 +0600

    taking input of an array from user.

---
+++ src/main/java/Chapter_04/UserInputArray.java
@@ -0,0 +1,24 @@
+package Chapter_04;
+
+import java.util.Scanner;
+
+public class UserInputArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Length of array: ");
+        int sizeOfArray = input.nextInt();
+
+        int[] numbers = new int[sizeOfArray];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(i + ": ");
+            numbers[i] = input.nextInt();
+        }
+
+        System.out.println("Index\tValue");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(i + "\t\t" + numbers[i]);
+        }
+    }
+}
