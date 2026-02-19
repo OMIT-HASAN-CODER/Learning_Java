a03aa8eaebcf98b7b76ac535f10628e9adec039c
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-02-20 01:02:57 +0600

    string joiner example.

--- src/main/java/Chapter_05/StringJoinerExample.java
+++ src/main/java/Chapter_05/StringJoinerExample.java
@@ -5,7 +5,7 @@ import java.util.StringJoiner;
 public class StringJoinerExample {
     public static void main(String[] args) {
         String[] cities = {"Dhaka", "Chittagong", "Khulna", "Mymensingh", "Rajshahi", "Rangpur", "Sylhet"};
-        StringJoiner joiner = new StringJoiner(", ");
+        StringJoiner joiner = new StringJoiner(", ", "(", ")");
         for (String city : cities) {
             joiner.add(city);
         }
