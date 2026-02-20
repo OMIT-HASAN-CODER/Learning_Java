24bbb6db3f17d440c070e8d25a34328fcd578fa6
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-02-21 01:06:54 +0600

    enum example.

---
+++ src/main/java/Chapter_05/EnumExample.java
@@ -0,0 +1,57 @@
+package Chapter_05;
+
+import java.security.PublicKey;
+
+public class EnumExample {
+    public enum Day {
+        SATURDAY, SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
+    }
+
+    public void displayDay(Day day) {
+        if (day == Day.SATURDAY) {
+            System.out.println("It's Saturday! Weekend!");
+        } else if (day == Day.FRIDAY) {
+            System.out.println("It's Friday! Weekend!");
+        } else {
+            System.out.println("Weekday!");
+        }
+    }
+
+    public enum Month {
+        JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+    }
+
+    public void displayMoth(Month month) {
+        switch (month) {
+            case OCTOBER:
+                System.out.println(month + " Is The Birthday Month.");
+                break;
+            default:
+                System.out.println(month + " Is Not the Birthday Month.");
+                break;
+        }
+    }
+
+    public enum Direction {
+        EAST, WEST, NORTH, SOUTH
+    }
+
+    public void displayAllDirection() {
+        Direction[] values = Direction.values();
+        for (Direction direction : values) {
+            System.out.println(direction);
+        }
+    }
+
+    public static void main(String[] args) {
+        EnumExample enumExample = new EnumExample();
+
+        Day day = Day.SATURDAY;
+        enumExample.displayDay(day);
+
+        Month month = Month.OCTOBER;
+        enumExample.displayMoth(month);
+
+        enumExample.displayAllDirection();
+    }
+}
