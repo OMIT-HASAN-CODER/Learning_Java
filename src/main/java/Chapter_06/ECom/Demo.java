f78dcaccbcffb6a8afb457fd9e045097ede76386
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-02-27 03:07:49 +0600

    do some change in ecommerce demo.

--- src/main/java/Chapter_06/ECom/Demo.java
+++ src/main/java/Chapter_06/ECom/Demo.java
@@ -4,9 +4,9 @@ public class Demo {
     public static void main(String[] args) {
         ShoppingCart shoppingCart = new ShoppingCart();
 
-        Product keyboard = new Product("Mechanical Gaming Keyboard", 48);
-        Product mouse = new Product("Gaming Mouse", 48);
-        Product ecoDot = new Product("Eco Dot (4th Generation)", 40);
+        Product keyboard = new Product("Mechanical Gaming Keyboard", 48, 10, 600, new Dimension(2, 10, 5));
+        Product mouse = new Product("Gaming Mouse", 48, 5, 150, new Dimension(2, 4, 3));
+        Product ecoDot = new Product("Eco Dot (4th Generation)", 40, 5, 300, new Dimension(10, 7, 8));
 
         shoppingCart.addProduct(keyboard);
         shoppingCart.addProduct(mouse);
