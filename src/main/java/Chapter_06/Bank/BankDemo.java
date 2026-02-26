1c9f6ff0be73a4cfb9b0084fa6c73f0fefb3ed29
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-02-27 03:01:32 +0600

    do some changes in bank app.

--- src/main/java/Chapter_06/Bank/BankDemo.java
+++ src/main/java/Chapter_06/Bank/BankDemo.java
@@ -3,14 +3,14 @@ package Chapter_06.Bank;
 public class BankDemo {
     public static void main(String[] args) {
         CheckingAccount checkingAccount = new CheckingAccount(1000000, "12345");
-        System.out.println(checkingAccount.getBalance());
+        System.out.println("Initial balance: " + checkingAccount.getBalance());
         checkingAccount.deposit(20000);
         checkingAccount.withdraw(10000);
 
-        SavingsAccount savingsAccount = new SavingsAccount(100000, "12345", 15);
-        System.out.println(savingsAccount.getBalance());
+        SavingsAccount savingsAccount = new SavingsAccount(100000, "12345", 8);
+        System.out.println("Initial balance:" + savingsAccount.getBalance());
+        System.out.println(savingsAccount.calculateInterest());
         savingsAccount.deposit(15000);
         savingsAccount.withdraw(1000);
-
     }
 }
--- src/main/java/Chapter_06/Bank/SavingsAccount.java
+++ src/main/java/Chapter_06/Bank/SavingsAccount.java
@@ -1,10 +1,28 @@
 package Chapter_06.Bank;
 
 public class SavingsAccount extends CheckingAccount {
+    private static final int MAX_WITHDRAWAL = 5;
+
     private final double interestRate;
+    private int withdrawalCount;
 
     public SavingsAccount(int initialBalance, String accountNumber, double interestRate) {
         super(initialBalance, accountNumber);
         this.interestRate = interestRate;
+    }
+
+    public double calculateInterest() {
+        System.out.print("Interest is: ");
+        return getBalance() * interestRate / 100;
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        if (withdrawalCount == MAX_WITHDRAWAL) {
+            System.out.println("Withdrawal limit exceed");
+        } else {
+            super.withdraw(amount);
+            withdrawalCount++;
+        }
     }
 }
